d05f6474f9a115a371c95b0969ead21aa6f12c2b
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2019-12-13 21:31:00 +0100

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+function openProject1() {
+    window.open("https://github.com/MichalUSER/astar-pathfinding-processing", '_blank');
+}
+
+function openProject2() {
+    window.open("https://github.com/MichalUSER/ecosystem-simulation", '_blank');
+}
+
+function openProject3() {
+    window.open("https://github.com/MichalUSER/micudb", '_blank');
+}
+
+function openProject4() {
+    window.open("https://github.com/MichalUSER/todo", '_blank');
+}
+
+function openProject5() {
+    window.open("https://github.com/MichalUSER/snake-processing", '_blank');
+}
